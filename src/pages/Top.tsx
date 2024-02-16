383aeadc9c6d1e7de282c5a2eb6d924b9fff022e
kikudeusyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-02-17 01:20:26 +0900

    feat: make Top page

--- src/pages/Top.tsx
+++ src/pages/Top.tsx
@@ -11,20 +11,30 @@ export const Top = ({ setPageName }: Props) => {
     <Container
       setPageName={setPageName}
       child={
-        <>
+        <div className="flex flex-col w-80 mx-auto pt-3 gap-8">
           <div>
-            <p>お知らせ</p>
-            <div>2024年2月16日 プレリリース</div>
-          </div>
-          <div>
-            <p>おすすめ</p>
-            <div>
-              <p>天然水2L</p>
-              <p>500円安い</p>
+            <h1 className="text-2xl font-bold">お知らせ</h1>
+            <div className="border-4 p-5">
+              <p>2024年2月16日 プレリリース</p>
             </div>
           </div>
-          <button>商品登録ページへ</button>
-        </>
+          <div>
+            <h2 className="text-2xl font-bold">おすすめ</h2>
+            <div className="flex flex-col  border-4 p-5 gap-2">
+              <div className="flex justify-between">
+                <p>天然水2L</p>
+                <p>500円安い</p>
+              </div>
+              <div className="flex justify-between">
+                <p>午後の紅茶2L</p>
+                <p>800円安い</p>
+              </div>
+            </div>
+          </div>
+          <button className="bg-slate-300 px-4 py-3 text-center  w-full mx-auto rounded-xl">
+            商品登録ページへ
+          </button>
+        </div>
       }
     />
   );
