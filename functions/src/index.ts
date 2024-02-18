83624ab7e8491b5715ad92b7527919d1a2382a4e
kikudeusyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-02-18 20:41:14 +0900

    feat: make functions

---
+++ functions/src/index.ts
@@ -0,0 +1,25 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+import { onRequest } from "firebase-functions/v2/https";
+import * as logger from "firebase-functions/logger";
+import { Item } from "./item/item";
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
+
+// export const helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", { structuredData: true });
+//   response.send("Hello from Firebase!");
+// });
+
+export const itemList = onRequest((request, response) => {
+  const item = new Item();
+  item.fetchCode();
+  logger.info("success");
+});
