03760b340f452dfcbbe0c6fb985caa2df0f2b98e
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-02-22 09:46:27 +0900

    feat: add item data in database

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,15 +7,35 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import { onRequest } from "firebase-functions/v2/https";
+// import { onRequest } from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 import YahooItem from "./item/yahooItem";
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const itemPrice = onRequest(async (request, response) => {
-  const item = new YahooItem({ janCode: "9784873115658", condition: "new" });
-  const price = await item.fetchPrice();
-  logger.info("Fetching price for item", +price);
-  response.status(200).json({ price });
+// import * as functions from "firebase-functions";
+import * as functions from "firebase-functions/v2";
+import * as admin from "firebase-admin";
+
+admin.initializeApp();
+const db = admin.firestore();
+
+export const itemPrice = functions.https.onRequest(async (req, res) => {
+  try {
+    const item = new YahooItem({ janCode: "9784873115658", condition: "new" });
+    const price = await item.fetchPrice();
+    logger.info("Fetching price for item", +price);
+    await db.collection("prices").add({
+      janCode: "9784873115658",
+      itemName: "test",
+      price: {
+        data: "2024/02/22",
+        price: price,
+      }
+    });
+    res.status(200).send("Data added successfully");
+  } catch (error) {
+    console.error("Error adding document: ", error);
+    res.status(500).send("Error adding document");
+  }
 });
