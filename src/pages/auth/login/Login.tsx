544fb2b67904fe8768fa57300d01549071e2a4ab
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-02-27 09:20:58 +0900

    refactor: modify img

--- src/pages/auth/login/Login.tsx
+++ src/pages/auth/login/Login.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { PageName } from "@/utils/helper/type";
 import LoginButton from "@/pages/auth/login/LoginButton";
+import padlock from "@/imgs/padlock.jpg";
 
 type Props = {
   setPageName: React.Dispatch<React.SetStateAction<PageName>>;
@@ -11,7 +12,7 @@ const Login = ({ setPageName }: Props) => {
   return (
     <div className="flex flex-col items-left w-80 mx-auto gap-8 py-8 text-left">
       <div className="flex flex-row items-center">
-        <img className="w-14 h-14" src="src/imgs/padlock.jpg" alt="" />
+        <img className="w-14 h-14" src={padlock} alt="" />
         <h1 className="text-3xl">ログイン</h1>
       </div>
       <div className="flex flex-col gap-4">
