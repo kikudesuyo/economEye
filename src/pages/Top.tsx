283fca373ea2a82ad76a6734c403724b3ad2b5e9
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-03-01 18:11:29 +0900

    feat: move page form top to itemList

--- src/pages/Top.tsx
+++ src/pages/Top.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import Button from "@/utils/components/Button";
 import { PageName } from "@/utils/helper/type";
 
 type Props = {
@@ -7,35 +7,45 @@ type Props = {
 
 const Top = ({ setPageName }: Props) => {
   return (
-    <div className="flex flex-col w-80 mx-auto pt-3 gap-8">
-      <div>
-        <h1 className="text-2xl font-bold">お知らせ</h1>
-        <div className="p-5 grid grid-cols-1 gap-4 border-2">
-          <ul className="list-disc pl-5 flex flex-col gap-4">
-            <li>2024年2月16日 プレリリース</li>
-            <li>2024年2月24日 スタイルの修正</li>
-          </ul>
-        </div>
-      </div>
-      <div>
-        <h2 className="text-2xl font-bold">おすすめ</h2>
-        <div className="flex flex-col  border-2 p-5 gap-2">
-          <div className="flex justify-between">
-            <p>天然水2L</p>
-            <p>いつもより500円安い</p>
-          </div>
-          <div className="flex justify-between">
-            <p>午後の紅茶2L</p>
-            <p>いつもより800円安い</p>
+    <div className="flex flex-col w-80 mx-auto mt-3">
+      <div className="flex flex-col gap-6">
+        <div>
+          <h1 className="text-2xl font-bold">お知らせ</h1>
+          <div className="p-5 grid grid-cols-1 gap-4 border-2">
+            <ul className="list-disc pl-5 flex flex-col gap-4">
+              <li>2024年2月16日 プレリリース</li>
+              <li>2024年2月24日 スタイルの修正</li>
+            </ul>
           </div>
         </div>
+        <div>
+          <h2 className="text-2xl font-bold">おすすめ</h2>
+          <div className="flex flex-col  border-2 p-5 gap-2">
+            <div className="flex justify-between">
+              <p>天然水2L</p>
+              <p>いつもより500円安い</p>
+            </div>
+            <div className="flex justify-between">
+              <p>午後の紅茶2L</p>
+              <p>いつもより800円安い</p>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col mt-14 gap-6">
+          <Button
+            label="商品登録ページ"
+            func={() => {
+              setPageName("RegisterItem");
+            }}
+          />
+          <Button
+            label="商品一覧ページ"
+            func={() => {
+              setPageName("ItemList");
+            }}
+          />
+        </div>
       </div>
-      <button
-        className="bg-slate-300 px-4 py-3 text-center  w-full mx-auto rounded-xl"
-        onClick={() => setPageName("RegisterItem")}
-      >
-        商品登録ページ
-      </button>
     </div>
   );
 };
