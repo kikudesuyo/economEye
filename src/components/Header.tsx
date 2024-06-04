7bc54efa3bbb25968e65601dcddaa127c19f0377
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-06-04 21:18:05 +0900

    to bold

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,17 +6,19 @@ const Header = () => {
   const navigate = useNavigate();
   return (
     <header className="sticky top-0 flex justify-between border-b border-black bg-white p-3">
-      <Link to="/" className="flex flex-col justify-center text-xl">
+      <Link to="/" className="flex flex-col justify-center text-xl font-bold">
         economEye👀
       </Link>
       <div className="flex gap-2">
         <Button
+          style="font-bold"
           label="ログイン"
           func={() => {
             navigate(PATHS.LOGIN);
           }}
         />
         <Button
+          style="font-bold"
           label="新規登録"
           func={() => {
             navigate(PATHS.SIGNUP);
