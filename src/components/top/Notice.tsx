7888ec2b5a9d6d351c65df0fdda0cdf4c2b61e89
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-06-10 17:26:35 +0900

    notice message

--- src/components/top/Notice.tsx
+++ src/components/top/Notice.tsx
@@ -4,8 +4,8 @@ const Notice = () => {
       <h1 className="text-2xl font-bold">お知らせ</h1>
       <div className="rounded-md border-2 border-gray-300 p-5 shadow-md">
         <ul className="flex list-disc flex-col gap-4 pl-5">
-          <li>2024年3月21日 プレリリース</li>
-          <li>2024年4月01日 スタイルの修正</li>
+          <li>2024年5月1日 プレリリース</li>
+          <li>2024年6月10日 デザインの修正</li>
         </ul>
       </div>
     </div>
