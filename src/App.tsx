38b424a0b2ec0ba7198eccba656411fa76d97065
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-06-13 11:39:01 +0900

    usecontext

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import "@/assets/styles/destyle.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
+import { AuthProvider } from "@/contexts/AuthContext";
+
 import Top from "@/pages/Top";
 import Home from "@/pages/Home";
 import Login from "@/pages/Login";
@@ -17,19 +19,21 @@ import { PATHS } from "@/utils/constant";
 function App() {
   return (
     <div className="flex min-h-screen flex-col">
-      <Router>
-        <ScrollToTop />
-        <Header />
-        <Routes>
-          <Route path={PATHS.HOME} element={<Home />} />
-          <Route path={PATHS.SIGNUP} element={<Signup />} />
-          <Route path={PATHS.LOGIN} element={<Login />} />
-          <Route path={PATHS.TOP} element={<Top />} />
-          <Route path={PATHS.REGISTER_ITEM} element={<RegisterItem />} />
-          <Route path={PATHS.ITEM_LIST} element={<ItemList />} />
-        </Routes>
-        <Footer />
-      </Router>
+      <AuthProvider>
+        <Router>
+          <ScrollToTop />
+          <Header />
+          <Routes>
+            <Route path={PATHS.HOME} element={<Home />} />
+            <Route path={PATHS.SIGNUP} element={<Signup />} />
+            <Route path={PATHS.LOGIN} element={<Login />} />
+            <Route path={PATHS.TOP} element={<Top />} />
+            <Route path={PATHS.REGISTER_ITEM} element={<RegisterItem />} />
+            <Route path={PATHS.ITEM_LIST} element={<ItemList />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </AuthProvider>
     </div>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,48 @@
 import { Link, useNavigate } from "react-router-dom";
 
+import { useAuth } from "@/contexts/useAuth";
 import Button from "@/components/Button";
 import { PATHS } from "@/utils/constant";
 
 const Header = () => {
+  const { isAuthenticated, logout } = useAuth();
   const navigate = useNavigate();
   return (
     <header className="sticky top-0 flex justify-between border-b border-gray-700 bg-white p-3 shadow-md">
-      <Link to="/" className="flex flex-col justify-center text-xl font-bold">
+      {/* <Link to="/" className="flex flex-col justify-center text-xl font-bold">
+        economEye👀
+      </Link> */}
+      <Link
+        to={isAuthenticated ? PATHS.TOP : PATHS.HOME}
+        className="flex flex-col justify-center text-xl font-bold"
+      >
         economEye👀
       </Link>
       <div className="flex gap-2">
-        <Button
-          label="ログイン"
-          func={() => {
-            navigate(PATHS.LOGIN);
-          }}
-        />
-        <Button
-          label="新規登録"
-          func={() => {
-            navigate(PATHS.SIGNUP);
-          }}
-        />
+        {isAuthenticated ? (
+          <Button
+            label="ログアウト"
+            func={() => {
+              logout();
+              navigate(PATHS.HOME);
+            }}
+          />
+        ) : (
+          <>
+            <Button
+              label="ログイン"
+              func={() => {
+                navigate(PATHS.LOGIN);
+              }}
+            />
+            <Button
+              label="新規登録"
+              func={() => {
+                navigate(PATHS.SIGNUP);
+              }}
+            />
+          </>
+        )}
       </div>
     </header>
   );
