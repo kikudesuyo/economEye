1b70f34ffc47a4640d9e9971b72fd5b75c0ce854
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-06-27 01:22:58 +0900

    define regular task

---
+++ functions/src/index.ts
@@ -0,0 +1,32 @@
+import {
+  onCall,
+  HttpsError,
+  CallableRequest,
+} from "firebase-functions/v2/https";
+import { onSchedule } from "firebase-functions/v2/scheduler";
+import { logger } from "firebase-functions";
+
+import { today } from "./utils/time";
+import { ClientParams } from "./utils/type";
+import { fetchItemData, setData } from "./helper/db";
+import { updateAllItems } from "./helper/updateAllItem";
+
+exports.registerNewItem = onCall(async (request: CallableRequest) => {
+  if (!request.auth) {
+    throw new HttpsError("unauthenticated", "User is not authenticated.");
+  }
+  const uid = request.auth.uid;
+  const data: ClientParams = request.data;
+  const itemData = await fetchItemData(data);
+  await setData(uid, itemData);
+  logger.info(`${data.itemName} is successfully registered: on ${today()}.`);
+  return { succuess: "success!" };
+});
+
+exports.updateItem = onCall(async () => {
+  return updateAllItems();
+});
+
+exports.runScheduledJobs = onSchedule("every day 00:00", async () => {
+  await updateAllItems();
+});
