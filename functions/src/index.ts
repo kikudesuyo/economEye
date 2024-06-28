4742ae3f6ad95e0ef73a2a170609241f8db45c6a
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-06-28 18:16:56 +0900

    setting region

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,23 +1,22 @@
-import { onCall, CallableRequest } from "firebase-functions/v2/https";
-import { onSchedule } from "firebase-functions/v2/scheduler";
+import { CallableRequest } from "firebase-functions/v2/https";
 
 import "module-alias/register";
 
 import { updateAllItems } from "@/helper/updateAllItem";
 import { registeItem } from "@/helper/registerNewItem";
+import { region } from "firebase-functions";
 
 // 新規商品登録
-exports.registerNewItem = onCall(async (request: CallableRequest) => {
-  return await registeItem(request);
-});
-
-// 商品情報の更新
-exports.runScheduledJobs = onSchedule(
-  {
-    schedule: "every day 00:00",
-    timeZone: "Asia/Tokyo",
-  },
-  async () => {
-    await updateAllItems();
+exports.registerNewItem = region("asia-northeast1").https.onCall(
+  async (request: CallableRequest) => {
+    return await registeItem(request);
   }
 );
+
+// 商品情報の更新
+exports.runScheduledJobs = region("asia-northeast1")
+  .pubsub.schedule("every day 00:00")
+  .timeZone("Asia/Tokyo")
+  .onRun(async () => {
+    await updateAllItems();
+  });
