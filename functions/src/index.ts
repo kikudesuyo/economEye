d8b6683d13c6ecbde4aea47c51dbd5f5dcd9591f
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-06-28 21:06:30 +0900

    feat: set region in asia-northeast1

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,5 @@
-import { region } from "firebase-functions";
-import { CallableRequest } from "firebase-functions/v2/https";
+import { CallableRequest, onCall } from "firebase-functions/v2/https";
+import { onSchedule } from "firebase-functions/v2/scheduler";
 
 import "module-alias/register";
 
@@ -7,16 +7,21 @@ import { updateAllItems } from "@/helper/updateAllItem";
 import { registerItem } from "@/helper/registerNewItem";
 
 // 新規商品登録
-exports.registerNewItem = region("asia-northeast1").https.onCall(
+exports.registerNewItem = onCall(
+  { region: "asia-northeast1" },
   async (request: CallableRequest) => {
     return await registerItem(request);
   }
 );
 
 // 商品情報の更新
-exports.runScheduledJobs = region("asia-northeast1")
-  .pubsub.schedule("every day 00:00")
-  .timeZone("Asia/Tokyo")
-  .onRun(async () => {
+exports.runScheduledJobs = onSchedule(
+  {
+    region: "asia-northeast1",
+    timeZone: "Asia/Tokyo",
+    schedule: "every day 00:00",
+  },
+  async () => {
     await updateAllItems();
-  });
+  }
+);
