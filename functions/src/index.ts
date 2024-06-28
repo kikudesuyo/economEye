38fd010cfb750da1d09fcd3803dc0400080122ed
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-06-28 21:20:50 +0900

    feat: rename

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,14 +6,14 @@ import "module-alias/register";
 import { updateAllItems } from "@/helper/updateAllItem";
 import { registerItem } from "@/helper/registerNewItem";
 import {
-  FUNCTION_REGION,
-  FUNCTION_TIMEZONE,
+  FUNCTIONS_REGION,
+  FUNCTIONS_TIMEZONE,
   ITEM_UPDATE_SCHEDULE,
 } from "@/constant/region";
 
 // 新規商品登録
 exports.registerNewItem = onCall(
-  { region: FUNCTION_REGION },
+  { region: FUNCTIONS_REGION },
   async (request: CallableRequest) => {
     return await registerItem(request);
   }
@@ -22,8 +22,8 @@ exports.registerNewItem = onCall(
 // 商品情報の更新
 exports.runScheduledJobs = onSchedule(
   {
-    region: FUNCTION_REGION,
-    timeZone: FUNCTION_TIMEZONE,
+    region: FUNCTIONS_REGION,
+    timeZone: FUNCTIONS_TIMEZONE,
     schedule: ITEM_UPDATE_SCHEDULE,
   },
   async () => {
