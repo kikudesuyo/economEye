aeb818ac8ea556808f687c1a8d9a286c3c27ec15
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-07-01 18:59:21 +0900

    refactor: use tempo

--- src/utils/timeUtils.ts
+++ src/utils/timeUtils.ts
@@ -1,9 +1,8 @@
+import { format } from "@formkit/tempo";
+
 export const today = (): string => {
   const date = new Date();
-  const year = String(date.getFullYear()).padStart(4, "0");
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const day = String(date.getDate()).padStart(2, "0");
-  return `${year}/${month}/${day}`;
+  return format(date, "YYYY/MM/DD");
 };
 
 export const formatDate = (dateString: string) => {
