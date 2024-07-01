c84244fbdff91b8f5dc25d0f348e93560ea9b836
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-07-01 18:46:15 +0900

    refactor: today function

--- functions/src/utils/time.ts
+++ functions/src/utils/time.ts
@@ -1,15 +1,14 @@
+import { format } from "@formkit/tempo";
+
 const getJapanTime = (): Date => {
   const now = new Date();
   const timezoneOffset = now.getTimezoneOffset();
-  const japanTimezoneOffset = timezoneOffset + (9 * 60);
-  const japanTime = new Date(now.getTime() + (japanTimezoneOffset * 60000));
-  return japanTime
-}
+  const japanTimezoneOffset = timezoneOffset + 9 * 60;
+  const japanTime = new Date(now.getTime() + japanTimezoneOffset * 60000);
+  return japanTime;
+};
 
 export const today = (): string => {
-  const date = getJapanTime();
-  const year = String(date.getFullYear()).padStart(4, "0");
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const day = String(date.getDate()).padStart(2, "0");
-  return `${year}/${month}/${day}`;
+  const jpDate = getJapanTime();
+  return format(jpDate, "YYYY/MM/DD");
 };
