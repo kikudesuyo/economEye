7c5df17a2be0b59520229d692b9a840665a11d7a
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-07-01 19:01:08 +0900

    refacotr: use tempo on formatDate

--- src/utils/timeUtils.ts
+++ src/utils/timeUtils.ts
@@ -7,7 +7,5 @@ export const today = (): string => {
 
 export const formatDate = (dateString: string) => {
   const dateObj = new Date(dateString);
-  const month = String(dateObj.getMonth() + 1).padStart(2, "0");
-  const day = String(dateObj.getDate()).padStart(2, "0");
-  return `${month}/${day}`;
+  return format(dateObj, "MM/DD");
 };
