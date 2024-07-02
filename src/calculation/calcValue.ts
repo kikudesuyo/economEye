9acf2329a582cee2126f3e8571596bbf886c1253
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-07-02 19:46:13 +0900

    refactor: omit redundant functions

--- src/calculation/calcValue.ts
+++ src/calculation/calcValue.ts
@@ -1,9 +1,5 @@
 import { ItemPriceValue } from "@/utils/types/items";
 
-const formatValue = (value: number): number => {
-  return Math.round(value);
-};
-
 const calcAverage = (prices: ItemPriceValue[]) => {
   const validPrices = prices.filter((price) => price !== null) as number[];
   const sum = validPrices.reduce((acc, cur) => acc + cur, 0);
@@ -12,7 +8,7 @@ const calcAverage = (prices: ItemPriceValue[]) => {
 
 export const formattedAverage = (prices: ItemPriceValue[]) => {
   const average = calcAverage(prices);
-  return formatValue(average);
+  return Math.round(average);
 };
 
 export const calcDeviationFromAverage = (
