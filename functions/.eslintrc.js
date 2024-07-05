5aeeae232b290951f880ec6936a1bdac7d0c3cf5
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-07-05 15:46:32 +0900

    feat: setting for test

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: ["tsconfig.json", "tsconfig.dev.json"],
+    project: ["tsconfig.json", "tsconfig.dev.json", "jest.config.js"],
     sourceType: "module",
   },
   ignorePatterns: [
