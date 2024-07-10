4d34750e1e06a5b860a0e5686005eac501ab5932
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-07-10 22:39:39 +0900

    refactor: flexible balse class in localStorage

---
+++ frontend/src/data/localStorage/localStorageService.ts
@@ -0,0 +1,19 @@
+export class LocalStorageService {
+  private storage: Storage;
+
+  constructor() {
+    this.storage = localStorage;
+  }
+
+  public saveToLocalStorage<T>(key: string, value: T) {
+    const itemData = JSON.stringify(value);
+    this.storage.setItem(key, itemData);
+  }
+  public getFromLocalStorage(key: string) {
+    const itemData = this.storage.getItem(key);
+    if (itemData === null) {
+      return [];
+    }
+    return JSON.parse(itemData);
+  }
+}
